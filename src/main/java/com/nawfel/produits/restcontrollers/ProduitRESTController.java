0de85942cc916c1426ec8ca11da45e842d6f54cb
NawfelHilal <nawfelhilal13@gmail.com>
2023-04-12 19:21:03 +0200

    Mise en place WebService REST methode CRUD

--- src/main/java/com/nawfel/produits/restcontrollers/ProduitRESTController.java
+++ src/main/java/com/nawfel/produits/restcontrollers/ProduitRESTController.java
@@ -3,11 +3,7 @@ package com.nawfel.produits.restcontrollers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.nawfel.produits.entities.Produit;
 import com.nawfel.produits.service.ProduitService;
 
@@ -24,4 +20,35 @@ public class ProduitRESTController {
 	{
 		return produitService.getAllProduits();
 	}
+	
+	@GetMapping("/{id}")
+	public Produit getProduitById(@PathVariable("id") Long id) 
+	{
+		return produitService.getProduit(id);
+	}
+	
+	@PostMapping
+	public Produit createProduit(@RequestBody Produit produit) 
+	{
+		return produitService.saveProduit(produit);
+	}
+
+	@PutMapping
+	public Produit updateProduit(@RequestBody Produit produit) 
+	{
+		return produitService.updateProduit(produit);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteProduit(@PathVariable("id") Long id)
+	{
+		produitService.deleteProduitById(id);
+	}
+
+	@GetMapping("/prodscat/{idCat}")
+	public List<Produit> getProduitsByCatId(@PathVariable("idCat") Long idCat) 
+	{
+		return produitService.findByCategorieIdCat(idCat);
+	}
+
 }
